5978636c2dbf4dc25000e699b0393c1f13b14156
Kloster <luciano.kloster@hotmail.com>
2022-08-29 02:34:37 -0300

    Arreglo

---
+++ js/parallax.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    
+    $(window).scroll(function(){
+        var windowWidth = $(window).width();
+
+        if(windowWidth > 800) {
+            var scroll = $(window).scrollTop();
+
+            $('header .texto').css({
+                'transform': 'translate(0px,' + scroll / 2 + '%)'
+            });
+
+            $('.acerca-de article').css({
+                'transform': 'translate(0px, -' + scroll / 4 + '%)'
+            });
+        }
+    });
+
+ 
+    $(window).scroll(function(){
+        var windowWidth = $(window).width();
+        if(windowWidth < 800){
+            $('.acerca-de article').css({
+                'transform': 'translate(0px,0px);'
+            });
+        }
+    });
+    
+});
